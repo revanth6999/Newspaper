e2ec910d32e74a17771db3393b051eb8352f15a2
revanth6999 <34978322+revanth6999@users.noreply.github.com>
2020-08-03 07:12:58 +0530

    Add files via upload

---
+++ NewspaperFormatter/Models/Interfaces/IFormatter.cs
@@ -0,0 +1,11 @@
+﻿using NewspaperFormatter.Models.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace NewspaperFormatter.Models.Interfaces
+{
+    public interface IFormatter
+    {
+        public List<String> format(Content content, NewspaperProperties newspaperProperties);
+    }
+}
