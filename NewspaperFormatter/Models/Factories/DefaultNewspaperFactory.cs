b6296627a29948fc351fb4b51e7c22c841b84756
revanth6999 <34978322+revanth6999@users.noreply.github.com>
2020-08-03 07:11:33 +0530

    Add files via upload

--- NewspaperFormatter/Models/Factories/DefaultNewspaperFactory.cs
+++ NewspaperFormatter/Models/Factories/DefaultNewspaperFactory.cs
@@ -1,12 +1,14 @@
 ﻿using NewspaperFormatter.Models.Concrete;
 using NewspaperFormatter.Models.Interfaces;
+using NewspaperFormatter.Models.Services;
+
 namespace NewspaperFormatter.Models.Factories
 {
     class DefaultNewspaperFactory : INewspaperFactory
     {
         public Newspaper makeNewspaper(Content content, NewspaperProperties newspaperProperties)
         {
-            return new Newspaper(content, newspaperProperties);
+            return new Newspaper(content, newspaperProperties, new RowsFormatter());
         }
     }
 }
